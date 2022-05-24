4db811cdfe3120abfaa22efcc8aa58b1ecf1e09b
TUSHARVERMA1113 <105992560+TUSHARVERMA1113@users.noreply.github.com>
2022-05-24 22:23:03 +0530

    Create mathe.py

---
+++ mathe.py
@@ -0,0 +1,6 @@
+import math as M    
+   
+# printing the floor value by using floor() function of math module  
+print ("The floor value of math.floor(-54.21) is: ", M.floor(-54.21))  
+print ("The floor value of math.floor(432.56) is: ", M.floor(432.56))  
+print ("The floor value of math.floor(320.62) is: ", M.floor(320.62))   
